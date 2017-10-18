492cd50bc61020f5d6b18e0151b0277ce6a497ea
davibrilhante <dbrilhante@land.ufrj.br>
2017-10-18 19:41:36 -0200

    new intersection method, trying to change coordinate system

--- triangulation/teste.py
+++ triangulation/teste.py
@@ -17,42 +17,48 @@ class Node:
                 self.wifiGain = 5
                 self.wifiTxPower = 20
 
+	def setBeams (self, nBeams):
+		#set node's number o beams/sectors
+		self.nBeams = nBeams
+		#set node's beamwidth
+		self.beamwidth = 2*pi/self.nBeams
 
-def projections(nbeams,r,node):
+
+def projections(r,node, adj):
 	precision = 3
 	Projection = []
-	alpha = 2*pi/nbeams
-        for n in range(nbeams):
-                if (n*alpha < pi) and (n*alpha < pi) and ((n+1)*alpha > pi):
-                        x1 = node[0]
-                        x2 = node[0] + r
+	alpha = 2*pi/node.nBeams
+        for n in range(node.nBeams):
+                if (n*alpha - adj < pi) and (n*alpha-adj < pi) and ((n+1)*alpha-adj > pi):
+                        x1 = node.x
+                        x2 = node.x + r
                 else:
-                        x1 = node[0] + r*math.cos(n*alpha)
-                        x2 = node[0] + r*math.cos((n+1)*alpha)
+                        x1 = node.x + r*math.cos(n*alpha - adj)
+                        x2 = node.x + r*math.cos((n+1)*alpha - adj)
                         #x = max(x1,x2) 
-		if x1< node[0] and x2<node[0]:
-			if x1<x2: x2 = node[0]
-			else: x1 = node[0]
-		elif x1> node[0] and x2>node[0]:
-			if x1<x2: x1 = node[0]
-			else: x2 = node[0] 
+		if x1< node.x and x2<node.x:
+			if x1<x2: x2 = node.x
+			else: x1 = node.x
+		elif x1> node.x and x2>node.x:
+			if x1<x2: x1 = node.x
+			else: x2 = node.x 
 
-                if (n*alpha < pi) and (n*alpha < (pi/2)) and ((n+1)*alpha > (pi/2)):
-                        y1 = node[1] + r
-                        y2 = node[1]
-                elif (n*alpha > pi) and (n*alpha < (3*pi/2)) and ((n+1)*alpha > (3*pi/2)):
-                        y1 = node[1]
-                        y2 = node[1] - r
+                if (n*alpha-adj < pi) and (n*alpha-adj < (pi/2)) and ((n+1)*alpha-adj > (pi/2)):
+                        y1 = node.y + r
+                        y2 = node.y
+                elif (n*alpha-adj > pi) and (n*alpha-adj < (3*pi/2)) and ((n+1)*alpha-adj > (3*pi/2)):
+                        y1 = node.y
+                        y2 = node.y - r
                 else:
-                        y1 = node[1] + r*math.sin(n*alpha)
-                        y2 = node[1] + r*math.sin((n+1)*alpha)
+                        y1 = node.y + r*math.sin(n*alpha-adj)
+                        y2 = node.y + r*math.sin((n+1)*alpha-adj)
 		
-		if y1< node[1] and y2<node[1]:
-                        if y1<y2: y2 = node[1]
-                        else: y1 = node[1]
-                elif y1> node[1] and y2>node[1]:
-                        if y1<y2: y1 = node[1]
-                        else: y2 = node[1]
+		if y1< node.y and y2<node.y:
+                        if y1<y2: y2 = node.y
+                        else: y1 = node.y
+                elif y1> node.y and y2>node.y:
+                        if y1<y2: y1 = node.y
+                        else: y2 = node.y
 
                 Projection.append([[round(x1,precision),round(x2,precision)],
 				   [round(y1,precision),round(y2,precision)]])
@@ -61,101 +67,66 @@ def projections(nbeams,r,node):
 
 
 def intersections(i, j, ri, rj):	#(arrayI, arrayJ):
-	d = math.hypot(i[0]-j[0], i[1]-j[1])
+	theta1 = math.atan2(j.y-i.y,j.x-i.x)
+	theta2 = math.atan2(i.y-j.y,i.x-j.x)
+	if theta1 < 0: theta1 = 2*pi + theta1
+	if theta2 < 0: theta2 = 2*pi + theta2
+
+	print math.degrees(theta1), math.degrees(theta2)
+	d = math.hypot(i.x-j.x, i.y-j.y)
 	delta = 0.25*(math.sqrt((d+ri+rj)*(d+ri-rj)*(d-ri+rj)*(-d+ri+rj)))
-	x1 = ((i[0]+j[0])/2) + ((j[0]-i[0])*((ri**2)-(rj**2))/(2*d**2)) + (2*(i[1]-j[1])*delta/(d**2))
-	x2 = ((i[0]+j[0])/2) + ((j[0]-i[0])*((ri**2)-(rj**2))/(2*d**2)) - (2*(i[1]-j[1])*delta/(d**2))
-	y1 = ((i[1]+j[1])/2) + ((j[1]-i[1])*((ri**2)-(rj**2))/(2*d**2)) - (2*(i[0]-j[0])*delta/(d**2))
-	y2 = ((i[1]+j[1])/2) + ((j[1]-i[1])*((ri**2)-(rj**2))/(2*d**2)) + (2*(i[0]-j[0])*delta/(d**2))
+	x1 = ((i.x+j.x)/2) + ((j.x-i.x)*((ri**2)-(rj**2))/(2*d**2)) + (2*(i.y-j.y)*delta/(d**2))
+	x2 = ((i.x+j.x)/2) + ((j.x-i.x)*((ri**2)-(rj**2))/(2*d**2)) - (2*(i.y-j.y)*delta/(d**2))
+	y1 = ((i.y+j.y)/2) + ((j.y-i.y)*((ri**2)-(rj**2))/(2*d**2)) - (2*(i.x-j.x)*delta/(d**2))
+	y2 = ((i.y+j.y)/2) + ((j.y-i.y)*((ri**2)-(rj**2))/(2*d**2)) + (2*(i.x-j.x)*delta/(d**2))
 
 	print x1,y1,x2,y2
-	alpha1 = math.atan2(y1 - i[1],x1 - i[0])
-	alpha2 = math.atan2(- i[1] + y2,-i[0] + x2)
+	alpha=[0,0,0,0]
+	alpha[0] = math.atan2(y1 - i.y,x1 - i.x)
+	alpha[1] = math.atan2(- i.y + y2,-i.x + x2)
 
-	beta1 = math.atan2(y1 - j[1],x1 - j[0])
-	beta2 = math.atan2(y2 - j[1],x2 - j[0])
+	alpha[2] = math.atan2(y1 - j.y,x1 - j.x)
+	alpha[3] = math.atan2(y2 - j.y,x2 - j.x)
 	
-	print math.degrees(alpha1), math.degrees(alpha2), math.degrees(beta1), math.degrees(beta2)
-	'''array = []
-	countI = 0
-	for i in arrayI:
-		countJ = 0
-		for j in arrayJ:
-			#x comparison
-			print
-			print "(",i[0][0],",",i[0][1],") , (",i[1][0],",",i[1][1],")"
-			print "(",j[0][0],",",j[0][1],") , (",j[1][0],",",j[1][1],")"
-			print
-			if i[0][0]<=j[0][0] or i[0][1]>=j[0][1]:
-				#y comparison
-				if i[1][0]<=j[1][0] or i[1][1]>=j[1][1]:
-					#ok, there's an intersection
-					intersectionX = [min(i[0][0],j[0][0]),max(i[0][1],j[0][1])]
-					intersectionY = [min(i[1][0],j[1][0]),max(i[1][1],j[1][1])]
-					array.append([countI, countJ, intersectionX, intersectionY])
-			countJ += 1
-		countI += 1			
-	return array'''
+	#print math.degrees(alpha[0]), math.degrees(alpha[1]), math.degrees(alpha[2]), math.degrees(alpha[3])
+	for m in range(len(alpha)):
+		if alpha[m] < 0:
+			alpha[m] = 2*pi + alpha[m]
+		print math.degrees(alpha[m])
+	distI = math.hypot(i.x-x2, i.y-y2)
+	distJ = math.hypot(j.x-x2, j.y-y2)
+	
+	deltaI = math.atan2(i.y-y2,i.x-x2) - theta1
+	deltaJ = math.atan2(j.y-y2,j.x-x2) - theta1
+	print math.degrees(deltaI), math.degrees(deltaJ)
+	
+	newI = Node(distI*math.cos(deltaI), distI*math.sin(deltaI), 10)
+	newJ = Node(distJ*math.cos(deltaJ), distJ*math.sin(deltaJ), 10)
+	newI.setBeams(i.nBeams)
+	newJ.setBeams(j.nBeams)
+	
+	print newI.x, newI.y, newJ.x, newJ.y
 
+	projectionsI = projections(d, newI, theta1)
+	projectionsJ = projections(d, newJ, theta1)
+	print projectionsI
+	print projectionsJ
 
 
 if __name__ == "__main__":
-	i = [0,0]
-	j = [3,3]
-	nbeamsI = 3
-	nbeamsJ = 8
-	r = math.hypot(i[0]-j[0], i[1]-j[1])
+	i = Node(0,0,10)#[0,0]
+	j = Node(3,3,10)
+	i.setBeams(3)
+	j.setBeams(8)
+	r = math.hypot(i.x-j.x, i.y-j.y)
         #Here starts an algorithm to detect an intersection betwen beams
         #The instersection of projections:
         #i is in quasi omni mode
         projectionsI = []
         projectionsJ = []
-        alpha = 2*pi/nbeamsI
-        '''for n in range(nbeamsI):
-		if (n*alpha < pi) and (n*alpha < pi) and ((n+1)*alpha > pi):
-			x1 = i[0]
-                        x2 = i[0] + r
-                else:
-                        x1 = i[0] + r*math.cos(n*alpha)
-                        x2 = i[0] + r*math.cos((n+1)*alpha)
-			#x = max(x1,x2)	
 
-                if (n*alpha < pi) and (n*alpha < (pi/2)) and ((n+1)*alpha > (pi/2)):
-			y1 = i[1] + r
-			y2 = i[1]
-                elif (n*alpha > pi) and (n*alpha < (3*pi/2)) and ((n+1)*alpha > (3*pi/2)):
-			y1 = i[1]
-			y2 = i[1] - r
-                else:
-                        y1 = i[1] + r*math.sin(n*alpha)
-                        y2 = i[1] + r*math.sin((n+1)*alpha)
-		projectionsI.append([[x1,x2],[y1,y2]])
-		#print "[",x1,",",x2,"] [",y1,",",y2,"]"
-	print projectionsI'''
-        #j is in normal mode
-        '''for n in range(nbeamsJ):
-		if (n*alpha < pi) and (n*alpha < pi) and ((n+1)*alpha > pi):
-			x1 = j[0]
-                        x2 = j[0] + r
-                else:
-                        x1 = j[0] + r*math.cos(n*alpha)
-                        x2 = j[0] + r*math.cos((n+1)*alpha)
-			#x = max(x1,x2)	
-
-                if (n*alpha < pi) and (n*alpha < (pi/2)) and ((n+1)*alpha > (pi/2)):
-			y1 = j[1] + r
-			y2 = j[1]
-                elif (n*alpha > pi) and (n*alpha < (3*pi/2)) and ((n+1)*alpha > (3*pi/2)):
-			y1 = j[1]
-			y2 = j[1] - r
-                else:
-                        y1 = j[1] + r*math.sin(n*alpha)
-                        y2 = j[1] + r*math.sin((n+1)*alpha)
-		projectionsJ.append([[x1,x2],[y1,y2]])
-	print projectionsJ'''
-
-	projectionsI = projections(nbeamsI, r, i)
-	projectionsJ = projections(nbeamsJ, r, j)
+	projectionsI = projections(r, i,0)
+	projectionsJ = projections(r, j,0)
 	print projectionsI
 	print projectionsJ
 	print intersections(i,j,r,r)#(projectionsI, projectionsJ)
